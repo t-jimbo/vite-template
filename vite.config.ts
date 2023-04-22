093a70bf3c35c0ae782fcf2aed24cd8bbed3e02a
t-jimbo <jbear.um@gmail.com>
2023-04-22 17:11:56 +0900

    chore: setup pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
+import Pages from "vite-plugin-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), Pages()],
 
   resolve: {
     alias: {
